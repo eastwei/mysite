1f42fc6acad96b20d7abf43b05711889d414e532
alana wei <alana.weidong@gmail.com>
2018-12-24 23:12:31 +0800

    print time in log

--- api/log.php
+++ api/log.php
@@ -1,8 +1,9 @@
 <?php
-class mylog{
-	/*输出log*/
-static public function setError($msg) {
-		error_log("slideshow.php ".$msg."\n",3,'/home/weidong/php.log');
-	}
-}
+        class mylog{
+                /*输出log*/
+                static public function setError($msg) {
+                        $str=date("Y-m-d H:i:s");
+                        error_log($str."--->".$msg."\n",3,"/var/tmp/php.log");
+                }
+        }
 ?>
