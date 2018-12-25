5d1cb5a4abf04353d6884b291971b966c474bfda
weidong <eastwei_2000@163.com>
2018-12-25 01:24:22 -0800

    extend log function

--- api/log.php
+++ api/log.php
@@ -1,9 +1,21 @@
 <?php
         class mylog{
-                /*输出log*/
-                static public function setError($msg) {
-                        $str=date("Y-m-d H:i:s");
-                        error_log($str."--->".$msg."\n",3,"/var/tmp/php.log");
-                }
+                /*send out log to file*/
+		static public function setError($msg) {
+
+			$time_str=date("Y-m-d H:i:s");
+			error_log($time_str."--->".$msg."\n",3,"/var/tmp/php.log");
+		}
+		/*send log to client*/
+		static public function var_json($info = '', $code = 10000, $data = '', $location = '') {
+
+			$out['code'] = $code ?: 0;
+			$out['info'] = $info ?: ($out['code'] ? 'error' : 'success');
+			$out['data'] = $data ?: '';
+			$out['location'] = $location;
+			header('Content-Type: application/json; charset=utf-8');
+			echo json_encode($out, JSON_HEX_TAG);
+			exit(0);
+		}
         }
 ?>
